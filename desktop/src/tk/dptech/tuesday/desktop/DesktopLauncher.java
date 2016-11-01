9e7994118e6ed0aef49a599689ebbf6f69d8dd8c
Austin White <Austin White>
2016-11-01 16:51:10 -0400

    Initial commit

---
+++ desktop/src/tk/dptech/tuesday/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tk.dptech.tuesday.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tk.dptech.tuesday.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
---
+++ html/src/tk/dptech/tuesday/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package tk.dptech.tuesday.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import tk.dptech.tuesday.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGdxGame();
+        }
+}
